8ffecb262c51d29751e2cbab559ba298f5678999
YousufTechnology <yousuf.tech.prog@gmail.com>
2020-04-07 22:55:43 +0400

    second comm

--- index.js
+++ index.js
@@ -5,5 +5,9 @@ app.get('/', (req, res)=> {
     return res.send({hi: "there"});
 });
 
+app.get('/hellow', (req, res)=> {
+    return res.send({hi: "there", t: "HELLLLL"});
+});
+
 const PORT = process.env.PORT || 5000 ;
 app.listen(PORT);
